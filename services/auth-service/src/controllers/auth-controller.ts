e424f77cc9540f48391551d43a97d84c0c50a00f
Qahraman H Taha <162983685+qahramantaha@users.noreply.github.com>
2026-04-19 18:35:53 +0000

    improve auth input validation rules

--- services/auth-service/src/controllers/auth-controller.ts
+++ services/auth-service/src/controllers/auth-controller.ts
@@ -9,10 +9,28 @@ export const registerUser = (req: Request, res: Response) => {
     });
   }
 
+  if (name.trim() === "") {
+    return res.status(400).json({
+      message: "Name cannot be empty"
+    });
+  }
+
+  if (!email.includes("@")) {
+    return res.status(400).json({
+      message: "Invalid email format"
+    });
+  }
+
+  if(password.length < 6) {
+    return res.status(400).json({
+      message: "Password must be at least 6 characters long"
+    });
+  }
+
   res.status(200).json({
     message: "Register request received",
     data: {
-      name,
+      name: name.trim(),
       email
     }
   });
@@ -24,6 +42,18 @@ export const loginUser = (req: Request, res: Response) => {
   if (!email || !password) {
     return res.status(400).json({
       message: "Email and password are required"
+    });
+  }
+
+  if (!email.includes("@")) {
+    return res.status(400).json({
+      message: "Invalid email format"
+    });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: "Password must be at least 6 characters long"
     });
   }
 
